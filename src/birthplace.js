737df38dd0e7207e4c7d3542129f1e9994f5756c
Wei-Ming Thor <twm213@hotmail.com>
2018-06-18 14:08:27 +0800

    Increase indent

--- src/birthplace.js
+++ src/birthplace.js
@@ -14,137 +14,137 @@ const REGION_MIDDLE_AMERICA = 'MIDDLE_AMERICA';
 const REGION_MISCELLANEOUS = 'MISCELLANEOUS';
 
 const stateCodePairs = {
-  'JHR': ['01', '21', '22', '24'],
-  'KDH': ['02', '25', '26', '27'],
-  'KTN': ['03', '28', '29'],
-  'MLK': ['04', '30'],
-  'NSN': ['05', '31', '59'],
-  'PHG': ['06', '32', '33'],
-  'PNG': ['07', '34', '35'],
-  'PRK': ['08', '36', '37', '38'],
-  'PLS': ['09', '40'],
-  'SGR': ['10', '41', '42', '43', '44'],
-  'TRG': ['11', '45', '46'],
-  'SBH': ['12', '47', '48', '49'],
-  'SWK': ['13', '50', '51', '52', '53'],
-  'KUL': ['14', '54', '55', '56', '57'],
-  'LBN': ['15', '58'],
-  'PJY': ['16'],
-  'UNKNOWN_STATE': ['82'],
+    'JHR': ['01', '21', '22', '24'],
+    'KDH': ['02', '25', '26', '27'],
+    'KTN': ['03', '28', '29'],
+    'MLK': ['04', '30'],
+    'NSN': ['05', '31', '59'],
+    'PHG': ['06', '32', '33'],
+    'PNG': ['07', '34', '35'],
+    'PRK': ['08', '36', '37', '38'],
+    'PLS': ['09', '40'],
+    'SGR': ['10', '41', '42', '43', '44'],
+    'TRG': ['11', '45', '46'],
+    'SBH': ['12', '47', '48', '49'],
+    'SWK': ['13', '50', '51', '52', '53'],
+    'KUL': ['14', '54', '55', '56', '57'],
+    'LBN': ['15', '58'],
+    'PJY': ['16'],
+    'UNKNOWN_STATE': ['82'],
 }
 
 const countryCodePairs = {
-  '60': { country: 'BN', region: REGION_SOUTHEAST_ASIA },
-  '61': { country: 'ID', region: REGION_SOUTHEAST_ASIA },
-  '62': { country: 'KH', region: REGION_SOUTHEAST_ASIA },
-  '63': { country: 'LA', region: REGION_SOUTHEAST_ASIA },
-  '64': { country: 'MM', region: REGION_SOUTHEAST_ASIA },
-  '65': { country: 'PH', region: REGION_SOUTHEAST_ASIA },
-  '66': { country: 'SG', region: REGION_SOUTHEAST_ASIA },
-  '67': { country: 'TH', region: REGION_SOUTHEAST_ASIA },
-  '68': { country: 'VN', region: REGION_SOUTHEAST_ASIA },
+    '60': { country: 'BN', region: REGION_SOUTHEAST_ASIA },
+    '61': { country: 'ID', region: REGION_SOUTHEAST_ASIA },
+    '62': { country: 'KH', region: REGION_SOUTHEAST_ASIA },
+    '63': { country: 'LA', region: REGION_SOUTHEAST_ASIA },
+    '64': { country: 'MM', region: REGION_SOUTHEAST_ASIA },
+    '65': { country: 'PH', region: REGION_SOUTHEAST_ASIA },
+    '66': { country: 'SG', region: REGION_SOUTHEAST_ASIA },
+    '67': { country: 'TH', region: REGION_SOUTHEAST_ASIA },
+    '68': { country: 'VN', region: REGION_SOUTHEAST_ASIA },
 
-  '71': { country: 'FOREIGN_UNKNOWN', region: null },
-  '72': { country: 'FOREIGN_UNKNOWN', region: null },
+    '71': { country: 'FOREIGN_UNKNOWN', region: null },
+    '72': { country: 'FOREIGN_UNKNOWN', region: null },
 
-  '74': { country: 'CN', region: REGION_EAST_ASIA },
-  '75': { country: 'IN', region: REGION_SOUTH_ASIA },
-  '76': { country: 'PK', region: REGION_SOUTH_ASIA },
-  '77': { country: 'SA', region: REGION_MIDDLE_EAST },
-  '78': { country: 'LK', region: REGION_SOUTH_ASIA },
-  '79': { country: 'BD', region: REGION_SOUTH_ASIA },
+    '74': { country: 'CN', region: REGION_EAST_ASIA },
+    '75': { country: 'IN', region: REGION_SOUTH_ASIA },
+    '76': { country: 'PK', region: REGION_SOUTH_ASIA },
+    '77': { country: 'SA', region: REGION_MIDDLE_EAST },
+    '78': { country: 'LK', region: REGION_SOUTH_ASIA },
+    '79': { country: 'BD', region: REGION_SOUTH_ASIA },
 
-  '83': {
-    country: 'AS|AU|CX|CC|CK|FJ|PF|GU|HM|MH|FM|NC|NZ|NU|NF|PG|TL|TK|UM|WF',
-    region: REGION_OCEANIA },
-  '84': {
-    country: 'AI|AR|AW|BO|BR|CL|CO|EC|GF|GP|GY|PY|PE|GS|ST|UY|VE',
-    region: REGION_SOUTH_AMERICA },
-  '85': {
-    country: 'DZ|AO|BW|BI|CM|CF|CG|CD|DG|EG|ER|ET|GA|GM|GN|KE|LR|MW|ML|MR|YT|' +
-             'MA|MZ|NA|NE|NG|RW|RE|SN|SL|SO|SD|SZ|TZ|TG|TO|TN|UG|ME|ZR|ZM|ZW',
-    region: REGION_AFRICA },
-  '86': {
-    country: 'AM|AT|BE|CY|DK|FO|FR|FI|DE|DD|GR|VA|IT|LU|' + 
-             'MK|MT|MC|NL|NO|PT|MD|SK|SI|ES|SE|CH|GG|JE|IM',
-    region: REGION_EUROPE },
-  '87': {
-    country: 'GB|IE',
-    region: REGION_BRITISH_ISLES },
-  '88': {
-    country: 'BH|IR|IQ|PS|JO|KW|OM|QA|YE|SY|TR|YE|YD|',
-    region: REGION_MIDDLE_EAST },
-  '89': {
-    country: 'JP|KP|KR|TW',
-    region: REGION_EAST_ASIA },
-  '90': {
-    country: 'BS|BB|BZ|CR|CU|DM|DO|SV|GD|GT|HT|HN|' +
-             'JM|MQ|MX|NI|PA|PR|KN|LC|VC|TT|TC|VI',
-    region: REGION_MIDDLE_AMERICA },
-  '91': {
-    country: 'CA|GL|AN|PM|US',
-    region: REGION_NORTH_AMERICA },
-  '92': {
-    country: 'AL|BY|BA|BG|HR|CZ|CS|EE|GE|HU|LV|LT|ME|PL|XK|RO|RU|RS|UA',
-    region: REGION_SOVIET_REPUBLIC },
-  '93': {
-    country: 'AF|AD|AQ|AG|AZ|BJ|BM|BT|IO|BF|CV|KY|KM|DY|GQ|TF|GI|GW|HK|' + 
-             'IS|CI|KZ|KI|KG|LS|LY|LI|MO|MG|MV|MU|MN|MS|NR|NP|MP|PW|PS|' +
-             'PN|SH|LC|VC|WS|SM|ST|SC|SB|SJ|TJ|TM|TV|HV|UZ|VU|VA|VG|YU',
-    region: REGION_MISCELLANEOUS },
-  
-  '98': { country: 'STATELESS', region: null },
-  '99': { country: 'UNSPECIFIED', region: null },
+    '83': {
+        country: 'AS|AU|CX|CC|CK|FJ|PF|GU|HM|MH|FM|NC|NZ|NU|NF|PG|TL|TK|UM|WF',
+        region: REGION_OCEANIA },
+    '84': {
+        country: 'AI|AR|AW|BO|BR|CL|CO|EC|GF|GP|GY|PY|PE|GS|ST|UY|VE',
+        region: REGION_SOUTH_AMERICA },
+    '85': {
+        country: 'DZ|AO|BW|BI|CM|CF|CG|CD|DG|EG|ER|ET|GA|GM|GN|KE|LR|MW|ML|MR|YT|' +
+                'MA|MZ|NA|NE|NG|RW|RE|SN|SL|SO|SD|SZ|TZ|TG|TO|TN|UG|ME|ZR|ZM|ZW',
+        region: REGION_AFRICA },
+    '86': {
+        country: 'AM|AT|BE|CY|DK|FO|FR|FI|DE|DD|GR|VA|IT|LU|' + 
+                'MK|MT|MC|NL|NO|PT|MD|SK|SI|ES|SE|CH|GG|JE|IM',
+        region: REGION_EUROPE },
+    '87': {
+        country: 'GB|IE',
+        region: REGION_BRITISH_ISLES },
+    '88': {
+        country: 'BH|IR|IQ|PS|JO|KW|OM|QA|YE|SY|TR|YE|YD|',
+        region: REGION_MIDDLE_EAST },
+    '89': {
+        country: 'JP|KP|KR|TW',
+        region: REGION_EAST_ASIA },
+    '90': {
+        country: 'BS|BB|BZ|CR|CU|DM|DO|SV|GD|GT|HT|HN|' +
+                'JM|MQ|MX|NI|PA|PR|KN|LC|VC|TT|TC|VI',
+        region: REGION_MIDDLE_AMERICA },
+    '91': {
+        country: 'CA|GL|AN|PM|US',
+        region: REGION_NORTH_AMERICA },
+    '92': {
+        country: 'AL|BY|BA|BG|HR|CZ|CS|EE|GE|HU|LV|LT|ME|PL|XK|RO|RU|RS|UA',
+        region: REGION_SOVIET_REPUBLIC },
+    '93': {
+        country: 'AF|AD|AQ|AG|AZ|BJ|BM|BT|IO|BF|CV|KY|KM|DY|GQ|TF|GI|GW|HK|' + 
+                'IS|CI|KZ|KI|KG|LS|LY|LI|MO|MG|MV|MU|MN|MS|NR|NP|MP|PW|PS|' +
+                'PN|SH|LC|VC|WS|SM|ST|SC|SB|SJ|TJ|TM|TV|HV|UZ|VU|VA|VG|YU',
+        region: REGION_MISCELLANEOUS },
+    
+    '98': { country: 'STATELESS', region: null },
+    '99': { country: 'UNSPECIFIED', region: null },
 };
 
 function numisBetween(num, lower, upper) {
-  return (num - lower) * (num - upper) <= 0
+    return (num - lower) * (num - upper) <= 0
 }
 
 function codeToState(code) {
-  return Object.keys(stateCodePairs).find(
-    key => stateCodePairs[key].includes(code));
+    return Object.keys(stateCodePairs).find(
+        key => stateCodePairs[key].includes(code));
 }
 
 function isMalaysia(code) {
-  return (
-    numisBetween(code, 1, 16) ||
-    numisBetween(code, 21, 59) ||
-    code == 82
-  );
+    return (
+        numisBetween(code, 1, 16) ||
+        numisBetween(code, 21, 59) ||
+        code == 82
+    );
 }
 
 function isForeign(code) {
-  return countryCodePairs[code] != undefined;
+    return countryCodePairs[code] != undefined;
 }
 
 function parseMalaysia(code) {  
-  return {
-    region: REGION_SOUTHEAST_ASIA,
-    country: 'MY',
-    state: codeToState(code),
-  };
+    return {
+        region: REGION_SOUTHEAST_ASIA,
+        country: 'MY',
+        state: codeToState(code),
+    };
 }
 
 function parseForeign(code) {
-  return Object.assign(
-    { state: null },
-    countryCodePairs[code]
-  );
+    return Object.assign(
+        { state: null },
+        countryCodePairs[code]
+    );
 }
 
 function parse(code) {
-  if (isMalaysia(code)) return parseMalaysia(code);
-  if (isForeign(code)) return parseForeign(code);
+    if (isMalaysia(code)) return parseMalaysia(code);
+    if (isForeign(code)) return parseForeign(code);
 
-  return null;
+    return null;
 }
 
 function isValid(code) {
-  return isMalaysia(code) || isForeign(code);
+    return isMalaysia(code) || isForeign(code);
 }
 
 module.exports = {
-  parse,
-  isValid,
+    parse,
+    isValid,
 }
--- src/index.js
+++ src/index.js
@@ -1,107 +1,103 @@
 const birthplace = require('./birthplace');
 
-function numisBetween(num, lower, upper) {
-  return (num - lower) * (num - upper) <= 0
-}
-
 // Check if date is before disregarding year.
 function dateIsBefore(before, max) {
-  const bNorm = new Date(0, before.getMonth(), before.getDate());
-  const mNorm = new Date(0, max.getMonth(), max.getDate());
+	const bNorm = new Date(0, before.getMonth(), before.getDate());
+	const mNorm = new Date(0, max.getMonth(), max.getDate());
 
-  return bNorm < mNorm;
+	return bNorm < mNorm;
 }
 
 function codeToDate(year, month, day) {
-  const today = new Date();
-  const birthDate = new Date(year, month - 1, day);
+	const today = new Date();
+	const birthDate = new Date(year, month - 1, day);
 
-  const age = today.getYear() - birthDate.getYear();
+	const age = today.getYear() - birthDate.getYear();
 
-  // Works for now. Update this in year 2099.
-  // For same year, checks if date has passed.
-  if (age > 100 || (age == 100 && dateIsBefore(birthDate, today))) {
-    birthDate.setFullYear(birthDate.getFullYear() + 100);
-  }
+	// Works for now. Update this in year 2099.
+	// For same year, checks if date has passed.
+	if (age > 100 || (age == 100 && dateIsBefore(birthDate, today))) {
+		birthDate.setFullYear(birthDate.getFullYear() + 100);
+	}
 
-  // Check valid date.
-  return (birthDate.getDate() == day)
-    ? birthDate
-    : NaN;
+	// Check valid date.
+	return (birthDate.getDate() == day)
+		? birthDate
+		: NaN;
 }
 
 function codeToGender(code) {
-  return (code % 2 == 0)
-    ? 'female'
-    : 'male';
+	return (code % 2 == 0)
+		? 'female'
+		: 'male';
 }
 
 function extractParts(icNum) {
-  const regex = /^(\d{2})(\d{2})(\d{2})-?(\d{2})-?(\d{3})(\d{1})$/;
-  const parts = icNum.match(regex);
+	const regex = /^(\d{2})(\d{2})(\d{2})-?(\d{2})-?(\d{3})(\d{1})$/;
+	const parts = icNum.match(regex);
 
-  if (!parts) {
-    throw new Error('Invalid MyKad number format');
-  }
+	if (!parts) {
+		throw new Error('Invalid MyKad number format');
+	}
 
-  return parts;
+	return parts;
 }
 
 function isValid(icNum) {
-  let parts;
+	let parts;
 
-  try {
-    parts =  extractParts(icNum);
-  } catch(error) {
-    return false;
-  }
+	try {
+		parts =  extractParts(icNum);
+	} catch(error) {
+		return false;
+	}
 
-  const birthDate = codeToDate(parts[1], parts[2], parts[3]);
-  return !isNaN(birthDate) && birthplace.isValid(parts[4]);
+	const birthDate = codeToDate(parts[1], parts[2], parts[3]);
+	return !isNaN(birthDate) && birthplace.isValid(parts[4]);
 }
 
 function parse(icNum, cb) {
-  let parts;
+	let parts;
 
-  try {
-    parts = extractParts(icNum);
-  } catch(error) {
-    return cb(error, null);
-  }
+	try {
+		parts = extractParts(icNum);
+	} catch(error) {
+		return cb(error, null);
+	}
 
-  return cb(null, {
-    birthDate: codeToDate(parts[1], parts[2], parts[3]),
-    birthPlace: birthplace.parse(parts[4]),
-    gender: codeToGender(parts[6])
-  })
+	return cb(null, {
+		birthDate: codeToDate(parts[1], parts[2], parts[3]),
+		birthPlace: birthplace.parse(parts[4]),
+		gender: codeToGender(parts[6])
+	});
 }
 
 function format(icNum, cb) {
-  let parts;
-  
-  try {
-    parts = extractParts(icNum);
-  } catch(error) {
-    return cb(error, null);
-  }
+	let parts;
+	
+	try {
+		parts = extractParts(icNum);
+	} catch(error) {
+		return cb(error, null);
+	}
 
-  const formatted = `${parts[1]}${parts[2]}${parts[3]}-${parts[4]}-${parts[5]}${parts[6]}`;
-  return cb(null, formatted);
+	const formatted = `${parts[1]}${parts[2]}${parts[3]}-${parts[4]}-${parts[5]}${parts[6]}`;
+	return cb(null, formatted);
 }
 
 function unformat(icNum, cb) {
-  format(icNum, (err, formatted) => {
-    if (err) {
-      return cb(err, null);
-    }
+	format(icNum, (err, formatted) => {
+		if (err) {
+			return cb(err, null);
+		}
 
-    return cb(null, formatted.replace(/-/g, ''));
-  });
+		return cb(null, formatted.replace(/-/g, ''));
+	});
 }
 
 module.exports = {
-  isValid,
-  parse,
-  format,
-  unformat,
+	isValid,
+	parse,
+	format,
+	unformat,
 }
